6b6d53945a603d38fbae9faa26d0737ba49a70a3
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-07-01 23:41:41 +0600

    Add start button to splash screen and navigate to main
    Replaces manual layout inflation with view binding in SplashScreenActivity. Adds a start button that, when clicked, launches MainActivity and finishes the splash screen.

--- app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
+++ app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
@@ -1,20 +1,27 @@
 package com.aas.medi_bridge
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aas.medi_bridge.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
