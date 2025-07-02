257b7a7b354ca39338b6ce185c4e1475f89ec58f
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-07-02 22:16:40 +0600

    Refactor splash screen and update color palette
    Removed view binding and button logic from SplashScreenActivity, simplifying the activity. Updated colors.xml by adding new green and grey shades and modifying the lightGrey color value.

--- app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
+++ app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
@@ -7,21 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.aas.medi_bridge.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashScreenBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.startBtn.setOnClickListener {
-            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
 }
