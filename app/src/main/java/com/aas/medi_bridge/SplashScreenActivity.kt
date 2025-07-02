4d3708df5ccb9df1c326ea9477246caec759ba09
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-07-02 23:06:08 +0600

    Add view binding to SplashScreenActivity
    Integrated ActivitySplashScreenBinding in SplashScreenActivity and set up the start button to launch MainActivity. Also fixed a typo in strings.xml ('appoinment' to 'appointment').

--- app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
+++ app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
@@ -7,13 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aas.medi_bridge.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+
+        binding.apply{
+            startBtn.setOnClickListener {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            }
+        }
 
 
     }
